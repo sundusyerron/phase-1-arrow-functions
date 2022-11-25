acf0809a95d9591ca2280aa2db4d60cae7dc0c2c
Sundus <sundusaden105@gmail.com>
2022-11-25 13:24:58 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+const divide = (parameter1, parameter2) => {
+    return 20
+}
+divide(2000, 100);
+
+
+
+const square = x => {
+     return x * x
+}
+square(4);
+
+const add = (a, b) => {
+   return a + b ;
+}
+add(3, 4);
